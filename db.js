112b54ae5fbbc035b2ac1174eaa82337d19b6b79
felipemir <robsonfelipeprog@gmail.com>
2025-05-28 09:46:36 -0300

    railll

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config(); // Adicione esta linha no topo se ainda não tiver
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost', // Fallback para localhost
+  host: process.env.DB_HOST || 'trolley.proxy.rlwy.net', // Fallback para localhost
   user: process.env.DB_USER || 'root',      // Fallback para seu usuário local
-  password: process.env.DB_PASSWORD || '',  // Fallback para sua senha local
-  database: process.env.DB_NAME || 'encosis2025', // Fallback para seu DB local
+  password: process.env.DB_PASSWORD || 'SswolwDlebjbAuznySNEUAeXHoIkETne',  // Fallback para sua senha local
+  database: process.env.DB_NAME || 'railway', // Fallback para seu DB local
   port: process.env.DB_PORT || 3306,        // Fallback para porta local
   waitForConnections: true,
   connectionLimit: 10,
