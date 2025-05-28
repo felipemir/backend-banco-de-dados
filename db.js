e138102755749bf508bc92ed8491df428d37f55d
felipemir <robsonfelipeprog@gmail.com>
2025-05-28 09:51:41 -0300

    featt

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config(); // Adicione esta linha no topo se ainda não tiver
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'trolley.proxy.rlwy.net', // Fallback para localhost
-  user: process.env.DB_USER || 'root',      // Fallback para seu usuário local
-  password: process.env.DB_PASSWORD || 'SswolwDlebjbAuznySNEUAeXHoIkETne',  // Fallback para sua senha local
-  database: process.env.DB_NAME || 'railway', // Fallback para seu DB local
-  port: process.env.DB_PORT || 3306,        // Fallback para porta local
+  host: process.env.MYSQLHOST || 'trolley.proxy.rlwy.net', // Fallback para localhost
+  user: process.env.MYSQLUSER || 'root',      // Fallback para seu usuário local
+  password: process.env.MYSQL_ROOT_PASSWORD || 'SswolwDlebjbAuznySNEUAeXHoIkETne',  // Fallback para sua senha local
+  database: process.env.MYSQL_DATABASE || 'railway', // Fallback para seu DB local
+  port: process.env.MYSQLPORT || 3306,        // Fallback para porta local
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
